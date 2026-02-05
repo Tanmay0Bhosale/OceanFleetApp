c54a81a89ac373adc568007f03c94d1fb1c28240
Tanmay <tanmaytanvi1234@gmail.com>
2026-02-05 10:40:08 +0530

    [Tanmay] : Added UC3: Implemented vessel search by vesselId

--- VesselUtil.java
+++ VesselUtil.java
@@ -2,18 +2,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 /*
- UC2: Vessel Storage Utility
- ---------------------------
- This class stores Vessel objects using a List.
- It supports dynamic addition of vessel records.
+ UC3: Retrieve Vessel by ID
+ -------------------------
+ This class allows searching a vessel
+ using a case-sensitive vesselId.
 */
 
 public class VesselUtil {
 
     private List<Vessel> vesselList = new ArrayList<>();
 
-    // Adds vessel to the list
     public void addVesselPerformance(Vessel vessel) {
         vesselList.add(vessel);
+    }
+
+    public Vessel getVesselById(String vesselId) {
+
+        // Loop to search vessel by ID
+        for (Vessel vessel : vesselList) {
+            if (vessel.getVesselId().equals(vesselId)) {
+                return vessel;
+            }
+        }
+        return null;
     }
 }
